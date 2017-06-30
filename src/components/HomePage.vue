a04e52e4f25aec2149e51833cc7bc0b909b270f7
Raashid A <raashid@wezile.com>
2017-06-30 17:49:05 +0530

    Home page changes (Added Search)

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,13 +2,73 @@
     <div class="container">
         
     <hr>
-    <div class="row landingicons">
-        <div class="col-4">
-            <i class="icon-beach"></i>
-            <span class="">Beach</span>
+
+    <div class="container">
+        <div class="d-flex justify-content-center searchcenter">
+            <div class="col-8 input-group">
+                <input type="text" class="form-control" style="height: 68px;" placeholder="Search for phots">
+                <span class="input-group-btn">
+                    <button class="btn btn-secondary" type="button">Go!</button>
+                </span>
+            </div>
         </div>
-        <div class="col-4">2</div>
-        <div class="col-4">3</div>
+    </div>
+
+
+    <div class="d-flex justify-content-center">
+        <span class="col-4 popu-search">Popular Search</span>
+    </div>
+    <div class="row landingicons">
+        <span class="cat-icon">
+            <router-link to="/cat/beach" data-reactid="17">
+                <span data-reactid="18">Beach</span>
+            </router-link>
+        </span>
+        <span class="cat-icon">
+            <router-link to="/cat/leopards" data-reactid="17">
+                <span data-reactid="18">Leopards</span>
+            </router-link>
+        </span>
+        <span class="cat-icon">
+            <router-link to="/cat/elephants" data-reactid="17">
+                <span data-reactid="18">Elephants</span>
+            </router-link>
+        </span>
+        <span class="cat-icon">
+            <router-link to="/cat/birds" data-reactid="17">
+                <span data-reactid="18">birds</span>
+            </router-link>
+        </span>
+        <span class="cat-icon">
+            <router-link to="/cat/knuckles" data-reactid="17">
+                <span data-reactid="18">Knuckles</span>
+            </router-link>
+        </span>
+        <span class="cat-icon">
+            <router-link to="/cat/Mountains" data-reactid="17">
+                <span data-reactid="18">Mountains</span>
+            </router-link>
+        </span>
+        <span class="cat-icon">
+            <router-link to="/cat/bear" data-reactid="17">
+                <span data-reactid="18">Bear</span>
+            </router-link>
+        </span>
+        <span class="cat-icon">
+            <router-link to="/cat/sinharaja" data-reactid="17">
+                <span data-reactid="18">Sinharaja</span>
+            </router-link>
+        </span>
+        <span class="cat-icon">
+            <router-link to="/cat/forst" data-reactid="17">
+                <span data-reactid="18">forest</span>
+            </router-link>
+        </span>
+        <span class="cat-icon">
+            <router-link to="/cat/kandy" data-reactid="17">
+                <span data-reactid="18">Kandy</span>
+            </router-link>
+        </span>
     </div>
     
     </div>
