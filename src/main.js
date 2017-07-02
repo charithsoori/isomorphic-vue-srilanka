8ce267630416770051891bd4be989a572d702179
Charith Sooriyaarachchi <charithsoori@gmail.com>
2017-07-02 22:27:31 +0530

    use bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue';
-import App from './App.vue';
 import VueRouter from 'vue-router';
 import { sync } from 'vuex-router-sync';
+import BootstrapVue from 'bootstrap-vue';
+import App from './App.vue';
 import store from './store'; // vuex store instance
 import router from './router'; // vue-router instance
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(VueRouter);
 sync(store, router); // done.
+
+// Globally register bootstrap-vue components
+Vue.use(BootstrapVue);
 
 new Vue({
   el: '#app',
